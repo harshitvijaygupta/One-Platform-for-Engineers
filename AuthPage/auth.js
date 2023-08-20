5ec4137e2acdd515aa6bf21eec844be9a8304776
Harshit Vijay Gupta <76866513+harshitvijaygupta@users.noreply.github.com>
2023-08-20 16:10:31 +0530

    Create auth.js

---
+++ AuthPage/auth.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const loginForm = document.getElementById('loginForm');
+    const signupForm = document.getElementById('signupForm');
+    
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const loginUsername = document.getElementById('loginUsername').value;
+        const loginPassword = document.getElementById('loginPassword').value;
+        const users = getUsersFromJson();
+        
+        const user = users.find(u => u.username === loginUsername && u.password === loginPassword);
+        
+        if (user) {
+            alert('Login successful');
+        } else {
+            alert('Invalid username or password');
+        }
+    });
+    
+    signupForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const signupUsername = document.getElementById('signupUsername').value;
+        const signupPassword = document.getElementById('signupPassword').value;
+        
+        if (!validateInput(signupUsername, signupPassword)) {
+            alert('Invalid input');
+            return;
+        }
+        
+        const newUser = { username: signupUsername, password: signupPassword };
+        const users = getUsersFromJson();
+        
+        if (isUsernameTaken(users, signupUsername)) {
+            alert('Username already taken');
+            return;
+        }
+        
+        users.push(newUser);
+        saveUsersToJson(users);
+        
+        alert('Signup successful');
+    });
+    
+    function getUsersFromJson() {
+        const jsonData = localStorage.getItem('data');
+        const data = JSON.parse(jsonData) || { users: [] };
+        return data.users;
+    }
+    
+    function saveUsersToJson(users) {
+        const data = { users: users };
+        localStorage.setItem('data', JSON.stringify(data));
+    }
+    
+    function validateInput(username, password) {
+        // Add your validation logic here
+        return username.length >= 3 && password.length >= 6;
+    }
+    
+    function isUsernameTaken(users, username) {
+        return users.some(u => u.username === username);
+    }
+});
